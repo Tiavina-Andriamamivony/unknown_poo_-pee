58a759af09c3d9b06f0d1bfa837d9d63a6f47252
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-07-26 11:23:52 +0300

    chore: Initialisation of the project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center  bg-background">
+        <h1 className="text-center font-bold text-4xl">Welcome to my template</h1>
+    </div>
+  );
+}
